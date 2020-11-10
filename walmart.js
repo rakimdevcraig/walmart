16175dd93638ad377f93f9c6c0ac49012e2b27fe
Rakim <rakimdevcraig@gmail.com>
2020-11-10 03:26:58 -0500

    first commit

---
+++ walmart.js
@@ -0,0 +1,67 @@
+
+
+
+//changed
+  // document.getElementById('add').onclick = function addToCart(){
+    // fetch("https://www.walmart.com/p13n/v1/walmart/postaddtocart/content", {
+    //   "headers": {
+    //     "accept": "application/json, text/javascript, */*; q=0.01",
+    //     "accept-language": "en-US,en;q=0.9",
+    //     "content-type": "application/json",
+    //     "sec-fetch-dest": "empty",
+    //     "sec-fetch-mode": "cors",
+    //     "sec-fetch-site": "same-origin",
+    //     "cookie": "brwsr=0f975d3f-1eaf-11eb-b3a7-42010a246e31; DL=02122%2C%2C%2Cip%2C02122%2C%2C; vtc=R-L834hoSqOCYICbweNmPQ; TBV=7; tb_sw_supported=true; location-data=02122%3ADorchester%3AMA%3A%3A1%3A1|1t1%3B%3B5.41%2C2gw%3B%3B8.73%2C1nf%3B%3B11.54%2C1my%3B%3B12.18%2C2ei%3B%3B12.24%2C1mf%3B%3B13.48%2C28o%3B%3B13.71%2C1pv%3B%3B14.5%2C21c%3B%3B14.95%2C28m%3B%3B17.67||7|1|1xtp%3B16%3B0%3B3.8%2C1xts%3B16%3B1%3B4.03%2C1xs0%3B16%3B2%3B4.17%2C1xrs%3B16%3B3%3B4.21%2C1xs1%3B16%3B4%3B4.39; bstc=XVAIePis6Li4tDRG0dNGsk; mobileweb=0; xpa=; xpm=3%2B1604988370%2BR-L834hoSqOCYICbweNmPQ~%2B0; cbp=436204236-1604990026946; ACID=373375da-2ddc-424d-b0e9-e4a26ba6e1b1; hasACID=1; auth=MTAyOTYyMDE4mSNhQthzTspfstP4aYfSjV7yIiN%2FOha621%2Bd%2BPZmPaaCDwg2%2FElOVy2BggvJvbjE9jFdWR5pNtoCV%2Fr7%2BPEheNeTmzmoYdAsObzgI9FKIslpEzr3WUufwrzgYLqMC3RA767wuZloTfhm7Wk2KcjygsxZsyqqU4iLObNl%2BMfESvnNMUudOjHRDsFLaiwTeFc5q6JXJrCMbxxFAHbMgXGlJP10BIhTLoK0hv9xaPMhQZsUMk70P8glgOEpLOprhDfMb%2FEw67%2FGsLtdlJHpe1JgEOsRc8mCSBTVrsk7BS5RUwsKETtiIDLIQAVrR8Nq7hSzYHmIuXMcQmrht5TvHMSLgVpEZ62yNLS%2BmBAD9R0ZPI3LI2S%2BAFt%2BGGPX7lgguYr%2F3g42iXiV1DdoqaK3mL2M5Q%3D%3D; type=GUEST; CRT=9e7fcfbb-c36f-47d1-8a06-764921418640; hasCRT=1; athrvi=RVI~h19fff2cc-h2426bd20-h6585aaf-h1a706975; TS013ed49a=01538efd7cbd1ae282b87e6b6f2f56c353138e37231505dcf0361485a41687847410093d3fe4be44464e9f8abf6524437416444af4; next-day=null|true|true|null|1604991267; TS01b0be75=01538efd7c37355d4acd0b8d97dff9aa2fa78746057c9a22c6dd5f72ecd7d19043f8a69d2a04183de2ba31477b3d937dd641c6a3cf; tb-c30=eus2-t1; akavpau_p2=1604991934~id=8ca7431d6e000cb56ac049171c364b59; akavpau_p8=1604991973~id=80462f01a1c1d6e9dbaf117342e50743; CXO_CART_ST=%7B%22mtoken%22%3A%22387%3A23%23203995458%232%3D77214874%237%3D220316045%22%7D; cart-item-count=1; com.wm.reflector=\"reflectorid:0000000000000000000000@lastupd:1604991379755@firstcreate:1604990058940\"; akavpau_p3=1604991979~id=a63db3b5454056069d841bace5d8a77b"
+    //   },
+    //   "referrer": "https://www.walmart.com/pac?id=0b48d414-c1b7-4cde-a357-16ad07159ca6&quantity=1&cv=5",
+    //   "referrerPolicy": "strict-origin-when-cross-origin",
+    //   "body": "{\"reqId\":\"030e281c-007-175b0f0e92bb10\",\"inflateContent\":true,\"modules\":[{\"moduleId\":\"43575554-4136-4147-938c-ad52ef37bc8b\",\"moduleType\":\"DefaultCarousel\",\"zone\":\"contentZone3\"}],\"userReqInfo\":{\"cartContext\":{\"items\":[{\"id\":\"436204236\",\"price\":299,\"pcp\":\"0:2636:1224908:1224995\",\"rhPath\":\"20000:26000:26005:26170:26775\",\"isNextDayEligible\":false}],\"threshold\":{\"balanceToThreshold\":0,\"thresholdType\":\"FreeShipping\"}}},\"userClientInfo\":{\"deviceType\":\"desktop\",\"ipAddress\":\"IP=0:0:0:0:0:0:0:1-0:0:0:0:0:0:0:1\",\"callType\":\"CLIENT\",\"zipCode\":\"02122\",\"isZipLocated\":true}}",
+    //   "method": "POST",
+    //   "mode": "no-cors"
+    // });
+    // }
+
+
+
+//unchanged
+    // document.getElementById('add').onclick = function addToCart(){
+    //   fetch("https://www.walmart.com/p13n/v1/walmart/postaddtocart/content", {
+    //     "headers": {
+    //       "accept": "application/json, text/javascript, */*; q=0.01",
+    //       "accept-language": "en-US,en;q=0.9",
+    //       "content-type": "application/json",
+    //       "credentials": "include",
+    //       "omitcsrfjwt": "true",
+    //       "sec-fetch-dest": "empty",
+    //       "sec-fetch-mode": "cors",
+    //       "sec-fetch-site": "same-origin",
+    //       "wm_site_mode": "0",
+    //       "cookie": "brwsr=0f975d3f-1eaf-11eb-b3a7-42010a246e31; DL=02122%2C%2C%2Cip%2C02122%2C%2C; vtc=R-L834hoSqOCYICbweNmPQ; TBV=7; tb_sw_supported=true; location-data=02122%3ADorchester%3AMA%3A%3A1%3A1|1t1%3B%3B5.41%2C2gw%3B%3B8.73%2C1nf%3B%3B11.54%2C1my%3B%3B12.18%2C2ei%3B%3B12.24%2C1mf%3B%3B13.48%2C28o%3B%3B13.71%2C1pv%3B%3B14.5%2C21c%3B%3B14.95%2C28m%3B%3B17.67||7|1|1xtp%3B16%3B0%3B3.8%2C1xts%3B16%3B1%3B4.03%2C1xs0%3B16%3B2%3B4.17%2C1xrs%3B16%3B3%3B4.21%2C1xs1%3B16%3B4%3B4.39; bstc=XVAIePis6Li4tDRG0dNGsk; mobileweb=0; xpa=; xpm=3%2B1604988370%2BR-L834hoSqOCYICbweNmPQ~%2B0; cbp=436204236-1604990026946; ACID=373375da-2ddc-424d-b0e9-e4a26ba6e1b1; hasACID=1; auth=MTAyOTYyMDE4mSNhQthzTspfstP4aYfSjV7yIiN%2FOha621%2Bd%2BPZmPaaCDwg2%2FElOVy2BggvJvbjE9jFdWR5pNtoCV%2Fr7%2BPEheNeTmzmoYdAsObzgI9FKIslpEzr3WUufwrzgYLqMC3RA767wuZloTfhm7Wk2KcjygsxZsyqqU4iLObNl%2BMfESvnNMUudOjHRDsFLaiwTeFc5q6JXJrCMbxxFAHbMgXGlJP10BIhTLoK0hv9xaPMhQZsUMk70P8glgOEpLOprhDfMb%2FEw67%2FGsLtdlJHpe1JgEOsRc8mCSBTVrsk7BS5RUwsKETtiIDLIQAVrR8Nq7hSzYHmIuXMcQmrht5TvHMSLgVpEZ62yNLS%2BmBAD9R0ZPI3LI2S%2BAFt%2BGGPX7lgguYr%2F3g42iXiV1DdoqaK3mL2M5Q%3D%3D; type=GUEST; CRT=9e7fcfbb-c36f-47d1-8a06-764921418640; hasCRT=1; athrvi=RVI~h19fff2cc-h2426bd20-h6585aaf-h1a706975; TS013ed49a=01538efd7cbd1ae282b87e6b6f2f56c353138e37231505dcf0361485a41687847410093d3fe4be44464e9f8abf6524437416444af4; next-day=null|true|true|null|1604991267; TS01b0be75=01538efd7c37355d4acd0b8d97dff9aa2fa78746057c9a22c6dd5f72ecd7d19043f8a69d2a04183de2ba31477b3d937dd641c6a3cf; tb-c30=eus2-t1; akavpau_p2=1604991934~id=8ca7431d6e000cb56ac049171c364b59; akavpau_p8=1604991973~id=80462f01a1c1d6e9dbaf117342e50743; CXO_CART_ST=%7B%22mtoken%22%3A%22387%3A23%23203995458%232%3D77214874%237%3D220316045%22%7D; cart-item-count=1; com.wm.reflector=\"reflectorid:0000000000000000000000@lastupd:1604991379755@firstcreate:1604990058940\"; akavpau_p3=1604991979~id=a63db3b5454056069d841bace5d8a77b"
+    //     },
+    //     "referrer": "https://www.walmart.com/pac?id=0b48d414-c1b7-4cde-a357-16ad07159ca6&quantity=1&cv=5",
+    //     "referrerPolicy": "strict-origin-when-cross-origin",
+    //     "body": "{\"reqId\":\"030e281c-007-175b0f0e92bb10\",\"inflateContent\":true,\"modules\":[{\"moduleId\":\"43575554-4136-4147-938c-ad52ef37bc8b\",\"moduleType\":\"DefaultCarousel\",\"zone\":\"contentZone3\"}],\"userReqInfo\":{\"cartContext\":{\"items\":[{\"id\":\"436204236\",\"price\":299,\"pcp\":\"0:2636:1224908:1224995\",\"rhPath\":\"20000:26000:26005:26170:26775\",\"isNextDayEligible\":false}],\"threshold\":{\"balanceToThreshold\":0,\"thresholdType\":\"FreeShipping\"}}},\"userClientInfo\":{\"deviceType\":\"desktop\",\"ipAddress\":\"IP=0:0:0:0:0:0:0:1-0:0:0:0:0:0:0:1\",\"callType\":\"CLIENT\",\"zipCode\":\"02122\",\"isZipLocated\":true}}",
+    //     "method": "POST",
+    //     "mode": "cors"
+    //   });
+    //   }
+  
+
+      document.getElementById('add').onclick = function addToCart(){
+      fetch("https://www.walmart.com/p13n/v1/walmart/postaddtocart/content", {
+        method: "POST",
+        headers: {
+          "accept": "application/json, text/javascript, */*; q=0.01",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/json",
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "cookie": "brwsr=0f975d3f-1eaf-11eb-b3a7-42010a246e31; DL=02122%2C%2C%2Cip%2C02122%2C%2C; vtc=R-L834hoSqOCYICbweNmPQ; TBV=7; tb_sw_supported=true; bstc=XVAIePis6Li4tDRG0dNGsk; mobileweb=0; xpa=; cbp=436204236-1604990026946; ACID=373375da-2ddc-424d-b0e9-e4a26ba6e1b1; hasACID=1; CRT=9e7fcfbb-c36f-47d1-8a06-764921418640; hasCRT=1; TS013ed49a=01538efd7c3fc43607a55cc84cb048b1f669674cbf3fec369d5451f6f84196639b4eee5e67d15c4b6909b9231b21c98aa069b8f313; akavpau_p2=1604995683~id=9477cee99c8f3808c79f84ec5f6030cf; _abck=fiz1oc5u0rg3kr8jpqy4_1855; auth=MTAyOTYyMDE4KUDyHrgnRUc7%2BDIvCxYNaZqs7%2Fr5SgzJbeykmVfYsDRXVg1VBmczxZQS4ADdgLFWF3Q3RlMiOXOEAnnsGOChuk9YXptqDS44VDez3iGKD%2FXxl773I%2B54vTjXFs8Snxk2RMvR4vpAGLQJj3IgMhxaykNe8Z3OwYPk5xgggL4ZNtveDM7kgqY5HxuP6w7QVix03mPB5OWVPHFoMzoAlkMV9lL6CZSzVZykBKTRtORKnJUU6ixnbyGwFO0AqYpfG29ldxHCTY9jnwiN%2FtUluKUe3jA3zySh6vaklFUxrvsunGASbPf5tfqO4qvgHXkYZA2Nun7VYW4I9FST0Et8UjTlYkaeGpNd%2FxQ%2FHoddnilVHW6xJpMeNlpKfSKwv7PiOCq9lpzDKHiOaSv1NwIhcPhFmQ%3D%3D; rtoken=MDgyNTUyMDE4Y%2Fv6HnHRE4rKCFeiNVDeuhiHmQkzbUMpuirA2i7IhJx2Kk5RuG5ezQ4Au6dCONm%2FVDP%2BJc4aDKkkrzlv%2BRGf8qOQol97XA8bVyt4jeGixy%2FypljPa79ytFzvZ%2F%2Fv4btb4xF4c%2Bjo1L67jBal7iXRvj1pzsbthRjAQzK%2FVxfduMzWGY5%2BHrCKmBW89MKT0khfeuAL25TNZlnFGd0fQdrkHfDTNvaj6YPRk8h%2FAm49WID4cPH2MU2PC1pLHks5S2OPEtCIqduYFNH0jHbJMjxuZjsZcETdi3PdrzVY7r%2FB8W5dWMUwdLSvTeIibwyBKwOUP3QpI6venEwCcqBYdkMHUYl%2BobRCrEj24t6A6GUmDb7SMHyEnYsoOw%2FKGu%2BoHGlIq8imYJkWHF5p18ZSsCRA3Q%3D%3D; SPID=294bc8af6b72ef97de150584b1a2d74275d2b9c021cb522d2df54ad491782b411f020f3ea36774a546677f10a58e9446wmjet; CID=9e8cd86c-50d8-489d-a294-b3d400540876; hasCID=1; customer=%7B%22firstName%22%3A%22Rakim%22%2C%22lastNameInitial%22%3A%22C%22%2C%22rememberme%22%3Atrue%7D; type=REGISTERED; WMP=4; xpm=3%2B1604995216%2BR-L834hoSqOCYICbweNmPQ~9e8cd86c-50d8-489d-a294-b3d400540876%2B0; x-csrf-jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0eXBlIjoiY29va2llIiwidXVpZCI6ImY4ZTRlMGMwLTIzMmEtMTFlYi1iZWI1LWEzYTM1OGUwNmM5MCIsImlhdCI6MTYwNDk5NTMwMywiZXhwIjoxNjA2Nzk1MzAzfQ.dxIxHZcRk6iIt7nYCM2RdDNkHlXgMYnSNupaVb3Xg28; next-day=null|true|true|null|1604995318; location-data=02122%3ADorchester%3AMA%3A%3A1%3A1|1t1%3B%3B5.41%2C2gw%3B%3B8.73%2C1nf%3B%3B11.54%2C1my%3B%3B12.18%2C2ei%3B%3B12.24%2C1mf%3B%3B13.48%2C28o%3B%3B13.71%2C1pv%3B%3B14.5%2C21c%3B%3B14.95%2C28m%3B%3B17.67||7|1|1xtp%3B16%3B0%3B3.8%2C1xts%3B16%3B1%3B4.03%2C1xs0%3B16%3B2%3B4.17%2C1xrs%3B16%3B3%3B4.21%2C1xs1%3B16%3B4%3B4.39; athrvi=RVI~h19fff2cc-h1a706975-h2426bd20-h6585aaf; akavpau_p8=1604996009~id=c749fc865279620a480be96229e84997; TS01b0be75=01538efd7c916fc684064bbd4160c64065eac0e940721faa98794e0752a92550b5451626fdee15cb174fdc46e43236706502fc5f20; CXO_CART_ST=%7B%22mtoken%22%3A%22387%3A23%23203997769%232%3D77217912%237%3D220319174%22%7D; tb-c30=wus-t1; cart-item-count=2; com.wm.reflector=\"reflectorid:0000000000000000000000@lastupd:1604995512735@firstcreate:1604990058940\"; akavpau_p3=1604996112~id=d0ac5c2b35c885e5d98445ac03fb25e0"
+  },
+        referrer: "https://www.walmart.com/pac?id=1796b138-3047-4aa9-8879-ed4d11b6930a&quantity=2&cv=10",
+        referrerPolicy: "strict-origin-when-cross-origin",
+        body: JSON.stringify("{\"reqId\":\"8d8ddb01-007-175b12ff99f9eb\",\"inflateContent\":true,\"modules\":[{\"moduleId\":\"192d0f85-2864-4efd-bb84-81749988b32d\",\"moduleType\":\"DefaultCarousel\",\"zone\":\"contentZone3\"}],\"userReqInfo\":{\"cartContext\":{\"items\":[{\"id\":\"436204236\",\"price\":299,\"pcp\":\"0:2636:1224908:1224995\",\"rhPath\":\"20000:26000:26005:26170:26775\",\"isNextDayEligible\":false}],\"threshold\":{\"balanceToThreshold\":0,\"thresholdType\":\"FreeShipping\"}}},\"userClientInfo\":{\"deviceType\":\"desktop\",\"ipAddress\":\"IP=0:0:0:0:0:0:0:1-0:0:0:0:0:0:0:1\",\"callType\":\"CLIENT\",\"zipCode\":\"02122\",\"isZipLocated\":true}}"),
+        mode: "no-cors"
+});
+}
